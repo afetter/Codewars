7ed1b9f2e86b798f650ebab7e38954a524beb3e6
Anderson Fetter <andersonf@humanforce.com>
2021-08-31 15:31:00 +1000

    Tests

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -9,7 +9,64 @@ namespace Codewars
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+            Console.WriteLine(string.Join(",", TowerBuilder(3)));
+        }
+        /// <summary>
+        /// Build Tower by the following given argument:
+        /// number of floors(integer and always greater than 0).
+        /// </summary>
+        /// <param name="nFloors"></param>
+        /// <returns>
+        /// [
+        /// '  *  ', 
+        /// ' *** ', 
+        /// '*****'
+        /// </returns>
+        public static string[] TowerBuilder(int nFloors)
+        {
+            var result = new string[nFloors];
+            var odd = 1;
+            var lenght = 1;
+            for (int i = 0; i < nFloors - 1; i++)
+            {
+                lenght += 2;
+            }
+
+            for (int i = 0; i < nFloors; i++)
+            {
+                var s = "*".PadLeft(odd, '*');
+                var mid = (lenght - s.Length) / 2;
+                result[i] = $"{"".PadLeft(mid, ' ')}{s}{"".PadRight(mid, ' ')}";
+                odd += 2;
+            }
+            
+            return result;
+        }
+
+        ////        Some numbers have funny properties.For example:
+        ////89 --> 8¹ + 9² = 89 * 1
+        ////695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+        ////46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+        ////Given a positive integer n written as abcd... (a, b, c, d...being digits) and a positive integer p
+        ////we want to find a positive integer k, if it exists, such as the sum of the digits of n taken to the successive powers of p is equal to k* n.
+        ////In other words:
+        ////Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n* k
+        ////If it is the case we will return k, if not return -1.
+        ////Note: n and p will always be given as strictly positive integers.
+        public static long digPow(int n, int p)
+        {
+            // your code
+            var mult = 0d;
+
+            foreach (char item in n.ToString())
+            {
+                mult += Math.Pow(Convert.ToInt32(item.ToString()), p);
+                p++;
+            }
+
+            if (mult % n != 0) return -1;
+
+            return (long)mult / n;
         }
 
         ////        Given an array of integers, find the one that appears an odd number of times.
@@ -22,16 +79,21 @@ namespace Codewars
         ////[1,2,2,3,3,3,4,3,3,3,2,2,1] shold return 4, because it appears 1 time(which is odd).
         public static int find_it(int[] seq)
         {
+            //Solution 1.
+            //Shows knowleged about Framework
             //var group = seq.GroupBy(s => s).FirstOrDefault(n => n.Count() % 2 != 0);
             //return group.Key;
 
+            //Solution 2.
+            //Shows knowleged about logic
             Dictionary<int, int> dic = new Dictionary<int, int>();
             foreach (var item in seq)
             {
                 if (!dic.ContainsKey(item))
                 {
                     dic.Add(item, 1);
-                } else
+                }
+                else
                 {
                     dic[item] = dic[item] + 1;
                 }
@@ -46,22 +108,22 @@ namespace Codewars
             return -1;
         }
 
-    
 
-    /// <summary>
-    /// 
-    ///Write a function, which takes a non-negative integer(seconds) as input and returns the time in a
-    ///human-readable format(HH:MM:SS)
-    //HH = hours, padded to 2 digits, range: 00 - 99
-    //MM = minutes, padded to 2 digits, range: 00 - 59
-    //SS = seconds, padded to 2 digits, range: 00 - 59
-    //The maximum time never exceeds 359999 (99:59:59)
-    /// </summary>
-    /// <param name="seconds"></param>
-    /// <returns></returns>
-    public static string GetReadableTime(int seconds)
+
+        /// <summary>
+        /// 
+        ///Write a function, which takes a non-negative integer(seconds) as input and returns the time in a
+        ///human-readable format(HH:MM:SS)
+        //HH = hours, padded to 2 digits, range: 00 - 99
+        //MM = minutes, padded to 2 digits, range: 00 - 59
+        //SS = seconds, padded to 2 digits, range: 00 - 59
+        //The maximum time never exceeds 359999 (99:59:59)
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public static string GetReadableTime(int seconds)
         {
-            if (seconds == 0) 
+            if (seconds == 0)
                 return "00:00:00";
 
             var fullHour = seconds / 3600m;
@@ -129,9 +191,9 @@ namespace Codewars
         public static int NbYear(int p0, double percent, int aug, int p)
         {
             int years = 0;
-            while(p0 < p)
+            while (p0 < p)
             {
-                p0 = (int)(p0 + p0 * (percent /100) + aug);
+                p0 = (int)(p0 + p0 * (percent / 100) + aug);
                 years++;
             }
             return years;
@@ -170,7 +232,7 @@ namespace Codewars
                     mult = 1;
                 }
 
-               
+
             }
 
             return count;
